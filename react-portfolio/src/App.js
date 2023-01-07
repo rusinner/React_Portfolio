3fa949393d0c068643060a3b0bfa41852673c50b
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-07 22:50:11 +0200

    Main Section css

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,31 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./components/Themes";
+import GlobalStyle from "./globalStyles";
+
+// components
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/work" element={<WorkPage />} />
+          <Route path="/skills" element={<MySkillsPage />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ react-portfolio/src/components/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  background: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+  position: relative;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+  }
+`;
+
+const Main = () => {
+  return <MainContainer>Main</MainContainer>;
+};
+
+export default Main;
