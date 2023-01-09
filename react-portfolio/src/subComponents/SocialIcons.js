306004cdd57bc25d3bec35619cbabae8035a2c1b
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-09 18:22:17 +0200

    svg social media changed

--- react-portfolio/src/subComponents/SocialIcons.js
+++ react-portfolio/src/subComponents/SocialIcons.js
@@ -2,26 +2,34 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { Github, Facebook, Instagram } from "../components/AllSvgs";
-
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
+  z-index: 3;
+`;
 const SocialIcons = () => {
   return (
-    <div>
+    <Icons>
       <div>
         <NavLink to="/">
-          <Github width={25} height={25} fill="currentColor" />
+          <Github width={35} height={35} fill="currentColor" />
         </NavLink>
       </div>
       <div>
         <NavLink to="/">
-          <Facebook width={25} height={25} fill="currentColor" />
+          <Facebook width={35} height={32} fill="currentColor" />
         </NavLink>
       </div>
       <div>
         <NavLink to="/">
-          <Instagram width={25} height={25} fill="currentColor" />
+          <Instagram width={35} height={35} fill="currentColor" />
         </NavLink>
       </div>
-    </div>
+    </Icons>
   );
 };
 
