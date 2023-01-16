a5ea66a60425c1cf3d7535921b746a2770078731
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-16 15:11:53 +0200

    card subcomponent

--- react-portfolio/src/components/WorkPage.js
+++ react-portfolio/src/components/WorkPage.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { darkTheme } from "./Themes";
+
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import { Work } from "../data/WorkData";
+import Card from "../subComponents/Card";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  overflow: hidden;
+`;
+const Main = styled.ul`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+  display: flex;
+  color: white;
+`;
 
 const WorkPage = () => {
-  return <div>Work</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <LogoComponent theme="light" />
+      <SocialIcons theme="voop" />
+      <PowerButton />
+
+      <Box>
+        <Main>
+          {Work.map((d) => (
+            <Card key={d.id} data={d} />
+          ))}
+        </Main>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default WorkPage;
---
+++ react-portfolio/src/subComponents/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = (props) => {
+  return <div>Work Data Cards</div>;
+};
+
+export default Card;
