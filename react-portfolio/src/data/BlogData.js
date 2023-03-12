d7edd779c0984f4a99668fb7836e79d27408d2ca
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-12 20:26:38 +0200

    package json and assets

---
+++ react-portfolio/src/data/BlogData.js
@@ -0,0 +1,69 @@
+export const Blogs = [
+  {
+    id: 1,
+    name: "Next js is the future of web",
+    tags: ["react", "next.js", "graphql"],
+    date: "17 February, 2023",
+    imgSrc:
+      "https://media.graphassets.com/output=format:jpg/resize=height:800,fit:max/CHz6Cbv8SsGpTqIl5Pbh",
+    link: "https://blog-next-app-phi.vercel.app/post/react-testing",
+  },
+  {
+    id: 2,
+    name: "How to create React Hamburger Menu using Styled-Components",
+    tags: ["react", "styled-components"],
+    date: "5 July, 2021",
+    imgSrc:
+      "https://media.graphassets.com/output=format:jpg/resize=height:800,fit:max/NIrPye5QU2UXJT0l7YNw",
+    link: "https://codebucks.hashnode.dev/react-hamburger-menu",
+  },
+  {
+    id: 3,
+    name: "React Loading Screen: Try these 3 cool loading screens for your app",
+    tags: ["react", "react-lottie", "styled-components"],
+    date: "6 July, 2021",
+    imgSrc: "",
+    link: "https://codebucks.hashnode.dev/react-loading-screens",
+  },
+  {
+    id: 4,
+    name: "How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+    tags: ["react", "redux", "framer-motion"],
+    date: "26 May, 2021",
+    imgSrc: "",
+    link: "https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1",
+  },
+  {
+    id: 5,
+    name: "What is Higher Order Component (HOC) in React JS?",
+    tags: ["react", "reactJS", "web-development"],
+    date: "3 Feb, 2021",
+    imgSrc: "",
+    link: "https://codebucks.hashnode.dev/what-is-higher-order-component",
+  },
+  {
+    id: 6,
+    name: "How to implement Pagination Component in React from scratch",
+    tags: ["react", "css"],
+    date: "5 Jan, 2021",
+    imgSrc: "",
+    link: "https://codebucks.hashnode.dev/pagination-in-react",
+  },
+
+  {
+    id: 7,
+    name: "What is Redux ? Simply Explained!",
+    tags: ["react", "redux", "javascript"],
+    date: "16 May, 2021",
+    imgSrc: "",
+    link: "https://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
+  },
+  {
+    id: 8,
+    name: "How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    tags: ["react", "framer-motion", "styled-components"],
+    date: "26 May, 2021",
+    imgSrc: "",
+    link: "https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3",
+  },
+];
