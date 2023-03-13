c3fcef2fdee1220c8678cb074e25ed34089e0064
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-13 22:41:16 +0200

    Update Work Data

--- react-portfolio/src/data/WorkData.js
+++ react-portfolio/src/data/WorkData.js
@@ -1,61 +1,92 @@
 export const Work = [
-    {
-        id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
-        
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
-    },
-    {
-        id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
-        
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
-        
-    },
-    {
-        id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
-        
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
-    },
-    {
-        id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
-       
-    },{
-        id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    
-  
-] 
+  {
+    id: 1,
+    name: "Developer's Portfolio Template",
+    description:
+      "React Portfolio Theme, using Sanity for Content management,framer motion for animations,scss",
+    tags: ["react", "sanity", "framer motion"],
+
+    demo: "https://vooprogue-template-portfolio-1.netlify.app/",
+    github: "https://github.com/rusinner/First_React_Portfolio",
+  },
+  {
+    id: 2,
+    name: "Crypto News App",
+    description:
+      "React Crypto App using Ant Design, Chart.js ,Redux Toolkit, and Rapid API for fetching Data.",
+    tags: ["react", "Ant Design", "Redux Toolkit", "RapidApi"],
+
+    demo: "https://vooprogue-crypto.netlify.app/",
+    github: "https://github.com/rusinner/React_Crypto_App",
+  },
+  {
+    id: 3,
+    name: "Full Dashboard App",
+    description:
+      "React dashboard App ,Syncfusion Components ,Tailwaind css . Theming options , Apps and Pages.",
+    tags: ["react", "Syncfusion", "tailwind"],
+
+    demo: "https://vooprogue-dashboard.netlify.app/",
+    github: "https://github.com/rusinner/React-Dashboard_App",
+  },
+  {
+    id: 4,
+    name: "Restaurant Website",
+    description:
+      "UI and Design React Restaurant Homepage,including Menu ,Gallery , Booking Table Option and About Section.",
+    tags: ["react", "styledComponents"],
+
+    demo: "https://vooprogue-restaurant.netlify.app/",
+    github: "https://github.com/rusinner/React_Restaurant_Website",
+  },
+  {
+    id: 5,
+    name: "Coffee Shop Website",
+    description:
+      "Html,sass,vanilla javascript website with shopping cart functionality.",
+    tags: ["javascript", "shopping cart", "sass"],
+
+    demo: "https://rusinner.github.io/Coffee_Shop_Website/",
+    github: "https://github.com/rusinner/Coffee_Shop_Website",
+  },
+  {
+    id: 6,
+    name: "Travel Agency Landing Page",
+    description:
+      "A Travel Agency Landing page build in vanilla javascript and sass using data-aos animation library.",
+    tags: ["vanilla js", "data-aos", "sass"],
+
+    demo: "https://rusinner.github.io/travel-landing-page/",
+    github: "https://github.com/rusinner/travel-landing-page",
+  },
+  {
+    id: 7,
+    name: "Gameverse Landing Page",
+    description:
+      "Built on top of Next js website using framer motion and tailwind css.",
+    tags: ["next js", "framer motion", "tailwind"],
+
+    demo: "https://next-animated-gaming-website.vercel.app/",
+    github: "https://github.com/rusinner/Next_Animated_Gaming_Website",
+  },
+  {
+    id: 8,
+    name: "Educational Content Landing Page",
+    description:
+      "A reponsive html,sass,vanilla javascript,multi pages website. Built from scratch.",
+    tags: ["vanilla js", "sass", "multipage"],
+
+    demo: "https://rusinner.github.io/Education_Website/",
+    github: "https://github.com/rusinner/Education_Website",
+  },
+  {
+    id: 9,
+    name: "TechSTore Ecommerce",
+    description:
+      "React Ecommerce App ,Next.js for file-based routing and data, using server-side rendering and static generation,sanity for content management and stripe integration for payments.",
+    tags: ["next js", "sanity", "stripe"],
+
+    demo: "https://react-ecommerce-app-ecru.vercel.app/",
+    github: "https://github.com/rusinner/React_Ecommerce_App",
+  },
+];
